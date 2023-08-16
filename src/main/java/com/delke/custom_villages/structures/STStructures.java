cbdee2a767ed37aae5f05e1a91110d8386123731
Bailey <delkerbailey@gmail.com>
2023-08-16 00:19:21 -0700

    Using Jigsaw Config

--- src/main/java/com/delke/custom_villages/structures/STStructures.java
+++ src/main/java/com/delke/custom_villages/structures/STStructures.java
@@ -1,9 +1,9 @@
-package com.delke.custom_villages;
+package com.delke.custom_villages.structures;
 
-import com.delke.custom_villages.structures.VillageBuildablePiece;
-import com.delke.custom_villages.structures.SkyStructures;
+import com.delke.custom_villages.Main;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,7 +14,6 @@ public class STStructures {
     /**
      * We are using the Deferred Registry system to register our structure as this is the preferred way on Forge.
      * This will handle registering the base structure for us at the correct time so we don't have to handle it ourselves.
-     *
      * HOWEVER, do note that Deferred Registries only work for anything that is a Forge Registry. This means that
      * configured structures and configured features need to be registered directly to BuiltinRegistries as there
      * is no Deferred Registry system for them.
@@ -25,7 +24,7 @@ public class STStructures {
      * Registers the base structure itself and sets what its path is. In this case,
      * this base structure will have the resourcelocation of structure_tutorial:sky_structures.
      */
-    public static final RegistryObject<StructureFeature<?>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", SkyStructures::new);
+    public static final RegistryObject<StructureFeature<JigsawConfiguration>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", SkyStructures::new);
 
     public static final DeferredRegister<StructurePieceType> REGISTER = DeferredRegister.create(Registry.STRUCTURE_PIECE_REGISTRY, Main.MODID);
 
