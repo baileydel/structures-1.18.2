21145a4d3b19ead106b0924936a978d48999280d
Bailey <delkerbailey@gmail.com>
2023-08-17 00:39:10 -0700

    Created a packet sent by client, forcing a structure to spawn.

---
+++ src/main/java/com/delke/custom_villages/network/ForcePacket.java
@@ -0,0 +1,138 @@
+package com.delke.custom_villages.network;
+
+import com.delke.custom_villages.Main;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.*;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.LevelHeightAccessor;
+import net.minecraft.world.level.StructureFeatureManager;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.RandomSupport;
+import net.minecraft.world.level.levelgen.WorldgenRandom;
+import net.minecraft.world.level.levelgen.XoroshiroRandomSource;
+import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureManager;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import static com.delke.custom_villages.Main.MODID;
+
+public class ForcePacket {
+    public static final ChunkPos STATIC_START = new ChunkPos(0, 0);
+    private static boolean loaded = false;
+
+    public ForcePacket() {}
+
+    public ForcePacket(FriendlyByteBuf buf) {}
+
+    public void write(FriendlyByteBuf buf) {}
+
+    /*
+        Only ever handle data on client (one way packet)
+     */
+    public static void handle(ForcePacket msg, Supplier<NetworkEvent.Context> context) {
+        NetworkEvent.Context ctx = context.get();
+        ctx.enqueueWork(() -> {
+            if (!loaded) {
+                loaded = true;
+                System.out.println("server handling");
+
+                if (ctx.getSender() != null) {
+                    ServerLevel level = ctx.getSender().getLevel();
+                    ConfiguredStructureFeature<?, ?> structureFeature = makeStructure();
+                    ChunkAccess chunkAccess = level.getChunk(STATIC_START.x, STATIC_START.z);
+
+                    if (structureFeature != null) {
+                        Main.poses.remove(STATIC_START);
+                        long seed = -234892394;
+                        StructureManager structureManager = level.getStructureManager();
+                        StructureFeatureManager featureManager = level.structureFeatureManager();
+                        RegistryAccess registryAccess = level.registryAccess();
+
+                        SectionPos sectionPos = SectionPos.of(STATIC_START, 0);
+                        ChunkGenerator chunkGenerator = level.getChunkSource().getGenerator();
+                        StructureStart start = tryGenerateStructure(structureFeature, chunkGenerator, featureManager, registryAccess, structureManager, seed, chunkAccess, STATIC_START, sectionPos);
+
+                        chunkAccess.setStartForFeature(structureFeature,start );
+                        chunkAccess.addReferenceForFeature(structureFeature, ChunkPos.asLong(0, 0));
+
+                        WorldgenRandom worldgenrandom = new WorldgenRandom(new XoroshiroRandomSource(RandomSupport.seedUniquifier()));
+
+                        if (start != StructureStart.INVALID_START) {
+                            BoundingBox box = getWritableArea(chunkAccess);
+
+                            featureManager.startsForFeature(sectionPos, structureFeature).forEach((p_211647_) -> {
+                                p_211647_.placeInChunk(level, featureManager, chunkGenerator, worldgenrandom, box, STATIC_START);
+                            });
+                        }
+                    }
+                }
+            }
+
+        });
+        ctx.setPacketHandled(true);
+    }
+
+    private static BoundingBox getWritableArea(ChunkAccess p_187718_) {
+        ChunkPos chunkpos = p_187718_.getPos();
+        int i = chunkpos.getMinBlockX();
+        int j = chunkpos.getMinBlockZ();
+        LevelHeightAccessor levelheightaccessor = p_187718_.getHeightAccessorForGeneration();
+        int k = levelheightaccessor.getMinBuildHeight() + 1;
+        int l = levelheightaccessor.getMaxBuildHeight() - 1;
+        return new BoundingBox(i, k, j, i + 15, l, j + 15);
+    }
+
+    private static ConfiguredStructureFeature<?, ?> makeStructure() {
+        Minecraft mc = Minecraft.getInstance();
+
+        if (mc.level != null && mc.getSingleplayerServer() != null) {
+            ServerLevel level = mc.getSingleplayerServer().getLevel(mc.level.dimension());
+
+            if (level != null) {
+                ResourceKey<ConfiguredStructureFeature<?, ?>> th = ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(MODID, "code_structure_sky_fan"));
+
+                return level.registryAccess().registryOrThrow(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY).getOrThrow(th);
+            }
+        }
+        return null;
+    }
+
+    private static StructureStart tryGenerateStructure(ConfiguredStructureFeature<?, ?> configuredstructurefeature, ChunkGenerator generator, StructureFeatureManager featureManager, RegistryAccess p_208019_, StructureManager p_208020_, long p_208021_, ChunkAccess access, ChunkPos p_208023_, SectionPos sectionPos) {
+        try {
+            int i = fetchReferences(featureManager, access, sectionPos, configuredstructurefeature);
+
+            HolderSet<Biome> holderset = configuredstructurefeature.biomes();
+            Predicate<Holder<Biome>> predicate = (holder) -> holderset.contains(adjustBiome(holder));
+
+            StructureStart structurestart =  configuredstructurefeature.generate(p_208019_, generator, generator.getBiomeSource(), p_208020_, p_208021_, p_208023_, i, access, predicate);
+            if (structurestart.isValid()) {
+                featureManager.setStartForFeature(sectionPos, configuredstructurefeature, structurestart, access);
+                return structurestart;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return StructureStart.INVALID_START;
+    }
+
+    private static int fetchReferences(StructureFeatureManager p_207977_, ChunkAccess p_207978_, SectionPos p_207979_, ConfiguredStructureFeature<?, ?> p_207980_) {
+        StructureStart structurestart = p_207977_.getStartForFeature(p_207979_, p_207980_, p_207978_);
+        return structurestart != null ? structurestart.getReferences() : 0;
+    }
+
+    protected static Holder<Biome> adjustBiome(Holder<Biome> p_204385_) {
+        return p_204385_;
+    }
+}
