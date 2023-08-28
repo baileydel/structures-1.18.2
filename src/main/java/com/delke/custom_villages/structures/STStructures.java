b2eb360a6a969e16bdff85b91ead20daeb990725
Bailey <delkerbailey@gmail.com>
2023-08-27 23:17:10 -0700

    Generates structure "road" now

--- src/main/java/com/delke/custom_villages/structures/STStructures.java
+++ src/main/java/com/delke/custom_villages/structures/STStructures.java
@@ -24,7 +24,7 @@ public class STStructures {
      * Registers the base structure itself and sets what its path is. In this case,
      * this base structure will have the resourcelocation of structure_tutorial:sky_structures.
      */
-    public static final RegistryObject<StructureFeature<JigsawConfiguration>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", SkyStructures::new);
+    public static final RegistryObject<StructureFeature<JigsawConfiguration>> SKY_STRUCTURES = DEFERRED_REGISTRY_STRUCTURE.register("road", SkyStructures::new);
 
     public static final DeferredRegister<StructurePieceType> REGISTER = DeferredRegister.create(Registry.STRUCTURE_PIECE_REGISTRY, Main.MODID);
 
