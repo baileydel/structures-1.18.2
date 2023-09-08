ad49a7d11c66b0f43a627cbb25b699f0d6588290
Bailey <delkerbailey@gmail.com>
2023-09-08 13:25:22 -0700

    + getPiece() + hasPiece() + removePiece()

--- src/main/java/com/delke/custom_villages/ModStructureManager.java
+++ src/main/java/com/delke/custom_villages/ModStructureManager.java
@@ -5,7 +5,9 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
 import net.minecraft.world.level.levelgen.structure.StructurePiece;
 import net.minecraft.world.level.levelgen.structure.StructureStart;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,20 +21,43 @@ public class ModStructureManager {
 
     public static Map<ChunkPos, List<StructureStart>> startMap = new HashMap<>();
 
+
+    @Nullable
+    public static StructurePiece getPiece(List<StructurePiece> pieces, String location) {
+        for (StructurePiece piece : pieces) {
+            if (piece instanceof BuildablePiece vbp) {
+                if (vbp.getElement().toString().split("\\[")[2].replace("]]", "").equals(location)) {
+                    return piece;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean hasPiece(List<StructurePiece> pieces, String location) {
+        return getPiece(pieces, location) != null;
+    }
+
     public static boolean hasPiece(ChunkPos pos, ConfiguredStructureFeature<?, ?> feature, String location) {
         List<StructureStart> starts = startMap.get(pos);
 
         for (StructureStart start : starts) {
             if (start.getFeature().equals(feature)) {
-                for (StructurePiece piece : start.getPieces()) {
-                    if (piece instanceof BuildablePiece vbp) {
-                        if (vbp.getElement().toString().split("\\[")[2].replace("]]", "").equals(location)) {
-                            return true;
-                        }
-                    }
+
+                if (hasPiece(start.getPieces(), location)) {
+                    return true;
                 }
             }
         }
         return false;
+    }
+
+    public static List<StructurePiece> removePiece(StructureStart start, String location) {
+        List<StructurePiece> out = new ArrayList<>(start.getPieces());
+
+        StructurePiece piece = getPiece(out, location);
+
+        out.remove(piece);
+        return out;
     }
 }
