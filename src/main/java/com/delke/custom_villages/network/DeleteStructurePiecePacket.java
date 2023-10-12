e8d95b55ff3332d85cff389f6960b0c9bc0f3b4d
Bailey <delkerbailey@gmail.com>
2023-10-11 22:53:14 -0700

    - createPiece()

--- src/main/java/com/delke/custom_villages/network/DeleteStructurePiecePacket.java
+++ src/main/java/com/delke/custom_villages/network/DeleteStructurePiecePacket.java
@@ -2,22 +2,12 @@ package com.delke.custom_villages.network;
 
 import com.delke.custom_villages.VillageStructureStartWrapper;
 import com.delke.custom_villages.mixin.StructureStartAccessor;
-import com.delke.custom_villages.structures.pieces.BuildablePiece;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.levelgen.LegacyRandomSource;
-import net.minecraft.world.level.levelgen.WorldgenRandom;
-import net.minecraft.world.level.levelgen.feature.StructureFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
 import net.minecraft.world.level.levelgen.structure.StructurePiece;
 import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraft.world.level.levelgen.structure.pieces.PiecesContainer;
-import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
-import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureManager;
 import net.minecraftforge.network.NetworkEvent;
 
 import java.util.ArrayList;
@@ -76,24 +66,5 @@ public class DeleteStructurePiecePacket {
         }
     }
 
-    private BuildablePiece createPiece() {
-        if (ResetStructurePacket.STRUCTURE_FEATURE != null) {
-            WorldgenRandom worldgenrandom = new WorldgenRandom(new LegacyRandomSource(0L));
 
-            worldgenrandom.setLargeFeatureSeed(ResetStructurePacket.SEED, ResetStructurePacket.STATIC_START.x, ResetStructurePacket.STATIC_START.z);
-            JigsawConfiguration jigsawconfiguration = (JigsawConfiguration) ResetStructurePacket.STRUCTURE_FEATURE.config;
-
-            StructureManager structuremanager = ResetStructurePacket.STRUCTURE_MANAGER;
-
-            StructureFeature.bootstrap();
-            Rotation rotation = Rotation.getRandom(worldgenrandom);
-            StructureTemplatePool structuretemplatepool = jigsawconfiguration.startPool().value();
-            StructurePoolElement structurepoolelement = structuretemplatepool.getRandomTemplate(worldgenrandom);
-
-            BlockPos pos = new BlockPos(30, 173, 0);
-
-            return new BuildablePiece(structuremanager, structurepoolelement, pos, structurepoolelement.getGroundLevelDelta(), rotation, structurepoolelement.getBoundingBox(structuremanager, pos, rotation));
-        }
-        return null;
-    }
 }
