6d9d7937cf730eaf2e4fc98d89f9ee60ad4fe26e
Bailey <delkerbailey@gmail.com>
2023-12-29 14:40:24 -0800

    Added Comments

--- src/main/java/com/delke/custom_villages/mixin/StructureStartMixin.java
+++ src/main/java/com/delke/custom_villages/mixin/StructureStartMixin.java
@@ -14,23 +14,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/**
- * @author Bailey Delker
- * @created 08/31/2023 - 11:19 AM
- * @project structures-1.18.2
+
+
+/*
+This mixin makes the pieceContainer in StructureStart.class Mutable
+Allowing us to change pieces within.
  */
 @Mixin(StructureStart.class)
 public abstract class StructureStartMixin {
 
     @Shadow @Mutable @Final private PiecesContainer pieceContainer;
 
-    /*
-            TODO This is only on creation of a structure
-             we want to make removing and adding pieces to a structure dynamic
-             TODO Instead of removing them after generation, how about we see how they are built....
-             and then add and remove from there
-             But this is good for testing, to even see if this is how we should do it
-         */
+
+    //TODO What does this do?
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(ConfiguredStructureFeature<?, ?> p_210077_, ChunkPos p_210078_, int p_210079_, PiecesContainer p_210080_, CallbackInfo ci) {
         boolean has = false;
