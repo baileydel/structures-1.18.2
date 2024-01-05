5ae889f3d96980a988c4dc75b9f625317a1915f2
Bailey <delkerbailey@gmail.com>
2024-01-05 02:01:11 -0800

    Renamed "StructureRenderPacket" to GetStructurePacket

--- src/main/java/com/delke/custom_villages/network/Network.java
+++ src/main/java/com/delke/custom_villages/network/Network.java
@@ -18,7 +18,7 @@ public class Network {
     );
 
     public static void init() {
-        INSTANCE.registerMessage(0, StructureRenderPacket.class, StructureRenderPacket::write, StructureRenderPacket::new, StructureRenderPacket::handle, Optional.of(NetworkDirection.PLAY_TO_CLIENT));
+        INSTANCE.registerMessage(0, GetStructurePacket.class, GetStructurePacket::write, GetStructurePacket::new, GetStructurePacket::handle, Optional.of(NetworkDirection.PLAY_TO_CLIENT));
         INSTANCE.registerMessage(1, AddPieceStructurePacket.class, AddPieceStructurePacket::write, AddPieceStructurePacket::new, AddPieceStructurePacket::handle);
         INSTANCE.registerMessage(2, GenerateStructurePacket.class, GenerateStructurePacket::write, GenerateStructurePacket::new, GenerateStructurePacket::handle, Optional.of(NetworkDirection.PLAY_TO_SERVER));
         INSTANCE.registerMessage(3, DeleteStructurePiecePacket.class, DeleteStructurePiecePacket::write, DeleteStructurePiecePacket::new, DeleteStructurePiecePacket::handle, Optional.of(NetworkDirection.PLAY_TO_SERVER));
