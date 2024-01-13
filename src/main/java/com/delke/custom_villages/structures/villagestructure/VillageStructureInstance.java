cb09433e495d1073528a6139d9ffb2acf0869d62
Bailey <delkerbailey@gmail.com>
2024-01-12 23:42:31 -0800

    Stores ChunkAccess under "chunk"

--- src/main/java/com/delke/custom_villages/structures/villagestructure/VillageStructureInstance.java
+++ src/main/java/com/delke/custom_villages/structures/villagestructure/VillageStructureInstance.java
@@ -2,8 +2,8 @@ package com.delke.custom_villages.structures.villagestructure;
 
 import com.delke.custom_villages.mixin.StructureStartAccessor;
 import com.delke.custom_villages.structures.StructureHandler;
+import com.delke.custom_villages.structures.pieces.BuildablePiece;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
@@ -11,7 +11,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.StructureFeatureManager;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
@@ -23,29 +22,47 @@ import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplie
 import net.minecraft.world.level.levelgen.structure.pieces.PiecesContainer;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureManager;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 
 import static com.delke.custom_villages.Main.MODID;
 
 public class VillageStructureInstance {
     private List<StructurePiece> pieces;
     private final StructureStart start;
-
     private PieceGeneratorSupplier.Context<?> context;
+    private final ChunkAccess chunk;
 
     public VillageStructureInstance(ServerLevel level, StructureStart start, ChunkAccess chunk) {
         this.start = start;
         this.pieces = start.getPieces();
+        this.chunk = chunk;
         createContext(level, start);
+    }
+
+    public void movePiece(String name, int x, int y, int z) {
+        List<StructurePiece> pieces1 = new ArrayList<>();
+
+        for (StructurePiece piece : this.pieces) {
+            if (piece instanceof BuildablePiece buildablePiece) {
+                if (buildablePiece.getName().equals(name)) {
+
+                    buildablePiece.move(x, z);
+                    savePieces(this.pieces);
+
+                    break;
+                }
+            }
+        }
+
     }
 
     public List<StructurePiece> getPieces() {
         return pieces;
     }
 
-    public void savePieces(ChunkAccess chunk, List<StructurePiece> pieces) {
+    public void savePieces(List<StructurePiece> pieces) {
         StructureHandler.INSTANCES.clear();
         this.pieces = pieces;
         ((StructureStartAccessor) (Object) start).setPieceContainer(new PiecesContainer(pieces));
@@ -55,6 +72,14 @@ public class VillageStructureInstance {
     public ChunkPos getChunkPos() {
         return start.getChunkPos();
     }
+
+
+
+
+
+
+
+
 
     public PieceGeneratorSupplier.Context<?> getContext() {
         return this.context;
@@ -67,13 +92,14 @@ public class VillageStructureInstance {
             ServerLevel level = mc.getSingleplayerServer().getLevel(mc.level.dimension());
 
             if (level != null) {
-                ResourceKey<ConfiguredStructureFeature<?, ?>> th = ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(MODID, "code_structure_sky_fan"));
+                ResourceKey<ConfiguredStructureFeature<?, ?>> th = ResourceKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(MODID, "new_village"));
 
                 return level.registryAccess().registryOrThrow(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY).getOrThrow(th);
             }
         }
         return null;
     }
+
     public void createContext(ServerLevel level, StructureStart start) {
         FeatureConfiguration config = start.getFeature().config;
 
@@ -87,8 +113,6 @@ public class VillageStructureInstance {
         StructureFeatureManager featureManager = level.structureFeatureManager();
         RegistryAccess registryAccess = featureManager.registryAccess();
 
-        Predicate<Holder<Biome>> predicate = (w) -> true;
-
-        this.context = new PieceGeneratorSupplier.Context<>(generator, biomeSource, VillageStructure.SEED, chunkPos, config, chunk, predicate, structureManager, registryAccess);
+        this.context = new PieceGeneratorSupplier.Context<>(generator, biomeSource, VillageStructure.SEED, chunkPos, config, chunk, (w) -> true, structureManager, registryAccess);
     }
 }
